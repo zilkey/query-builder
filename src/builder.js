d64f0f2380baddf085a1825a5f63e7fba4bc33a6
Jeff Dean <jeff@galvanize.com>
2016-07-07 14:58:38 -0600

    implemented where and insert

--- src/builder.js
+++ src/builder.js
@@ -5,18 +5,54 @@ module.exports = connection;
 function connection(config){
   function builder(tableName){
     let columns = [];
-
+    let sql = [];
+    let bindings = [];
+    let action = "select";
     return {
-      toString: function () {
-        return `select ${columns.map(e => `"${e}"`).join(", ")} from ${tableName}`
+      toSQL: function () {
+        let columnString, string;
+        switch (action) {
+          case 'select':
+            columnString = columns.length ? columns.map(e => `"${e}"`).join(", ") : "*"
+            string = `select ${columnString} from ${tableName}`
+            if (sql.length) string += ` where ${sql.map(clause => `"${clause}" = ?`).join(" and ")}`
+            break;
+
+          case 'insert' :
+            columnString = columns.map(e => `"${e}"`).join(", ")
+            string = `insert into ${tableName} (${columnString}) values (${bindings.map(binding => "?").join(", ")})`
+            break;
+        }
+
+        return { sql: string, bindings }
       },
       select: function(...cols){
         columns = columns.concat(cols)
         return this;
       },
       then: function(callback){
-        return builder.raw(this.toString()).then(callback)
+        let statement = this.toSQL();
+        return builder.raw(statement.sql, statement.binds).then(callback)
       },
+      where: function(args){
+        for (var val in args) {
+          if (args.hasOwnProperty(val)) {
+            bindings.push(args[val]);
+            sql.push(val)
+          }
+        }
+        return this;
+      },
+      insert: function(args){
+        action = "insert"
+        for (var val in args) {
+          if (args.hasOwnProperty(val)) {
+            bindings.push(args[val]);
+            columns.push(val)
+          }
+        }
+        return this;
+      }
     }
   }
 
--- test/builder.test.js
+++ test/builder.test.js
@@ -12,13 +12,38 @@ describe("builder", function () {
 
     it("builds a select statement", function () {
       let statement = builder('users').select('id', 'name');
-      expect(statement.toString()).to.eq('select "id", "name" from users');
+      expect(statement.toSQL()).to.deep.eq({sql: 'select "id", "name" from users', bindings: []});
     });
 
     it("it allows selects to be chained", function(){
       let statement = builder('todos').select('id', 'name').select('foo');
-      expect(statement.toString()).to.eq('select "id", "name", "foo" from todos');
+      expect(statement.toSQL()).to.deep.eq({sql: 'select "id", "name", "foo" from todos', bindings: []});
     })
+  })
+
+  describe("where", function () {
+    it("builds a where statement", function () {
+      let statement = builder('users').where({name: 'Joe', age: 18});
+      expect(statement.toSQL()).to.deep.eq({sql: `select * from users where "name" = ? and "age" = ?`, bindings: ['Joe', 18]});
+    });
+  })
+
+  describe("insert", function () {
+    it("builds an insert statement", function () {
+      let statement = builder('users').insert({name: 'Joe', age: 18});
+      expect(statement.toSQL()).to.deep.eq({sql: `insert into users ("name", "age") values (?, ?)`, bindings: ['Joe', 18]});
+    });
+  })
+
+  describe("chaining", function () {
+    it("allows clauses to be added in any order", function () {
+      let statement = builder('users')
+        .where({name: 'Tom'})
+        .select("foo", "bar")
+        .where({age: 34})
+
+      expect(statement.toSQL()).to.deep.eq({sql: `select "foo", "bar" from users where "name" = ? and "age" = ?`, bindings: ['Tom', 34]});
+    });
   })
 
   describe("then", function () {
