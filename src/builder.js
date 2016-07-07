99744cfdc89ce94c3a19e3a43b9ca7ca83ab3871
Jeff Dean <jeff@galvanize.com>
2016-07-07 13:36:12 -0600

    builder can execute basic queries against a hard-coded connection

--- src/builder.js
+++ src/builder.js
@@ -8,6 +8,22 @@ module.exports = function(tableName){
     select: function(...cols){
       columns = columns.concat(cols)
       return this;
+    },
+    then: function(callback){
+      let builder = this;
+      return new Promise(function(resolve, reject){
+        var pg = require('pg');
+        var client = new pg.Client({
+          database: 'builder-test',
+        });
+        client.connect(function (err) {
+          if (err) return reject(err);
+          client.query(builder.toString(), [], function (err, result) {
+            if (err) return reject(err);
+            resolve(result.rows);
+          })
+        })
+      }).then(callback)
     }
   }
 
--- test/builder.test.js
+++ test/builder.test.js
@@ -13,9 +13,33 @@ describe("builder", function () {
     it("it allows selects to be chained", function(){
       let statement = builder('todos').select('id', 'name').select('foo');
       expect(statement.toString()).to.eq('select "id", "name", "foo" from todos');
-
     })
-
   })
+
+  describe("then", function () {
+    it("returns a promise which yields the results", function (done) {
+      var pg = require('pg');
+      var client = new pg.Client({
+        database: 'builder-test',
+      });
+      client.connect(function (err) {
+        if (err) throw err;
+        client.query('insert into users (name, age) values ($1, $2)', ['John', 24], function (err, result) {
+          if (err) throw err;
+
+          let statement = builder('users').select('name', 'age');
+          statement.then(function (rows) {
+            expect(rows).to.deep.equal([
+              {name: 'John', age: 24},
+            ])
+            done()
+          }, done)
+
+        });
+      });
+    });
+  })
+
+
 
 })
